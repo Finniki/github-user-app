d14ff54451cb486e29af47c820901b3e4b718fc4
Finn <mailtorufina@gmail.com>
2022-10-21 12:35:30 -0700

    update source code

--- src/components/UserInterface/SearchBar/SearchBar.js
+++ src/components/UserInterface/SearchBar/SearchBar.js
@@ -1,34 +1,18 @@
 import styles from "./SearchBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRef } from "react";
 
 const SearchBar = (props) => {
-  const inputRef = useRef();
-  console.log("Ref:", inputRef);
-
-  const clickHandler = async () => {
-    //retrieve an store user input
-    const value = inputRef.current.value;
-    console.log("ref: ", value);
-    
-    //call the function from app.js and pass the value in
-    props.onClick(value);
-    inputRef.current.value = " ";
-    
-  };
-
   return (
     <div>
       <label htmlFor="search" className={styles.label}>
         <FontAwesomeIcon icon={props.icon} className={styles.icon} />
         <input
-          ref={inputRef}
           id="search"
           type="text"
           placeholder="Search GitHub username_"
           className={styles.input}
         />
-        <button onClick={clickHandler} type="submit" className={styles.btn}>
+        <button type="submit" className={styles.btn}>
           Search
         </button>
       </label>
