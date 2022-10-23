1354e7bf80c939140ea0a79b65dfb4d677168ec2
Finn <mailtorufina@gmail.com>
2022-10-23 13:15:14 -0700

    add click handler

--- src/components/UserInterface/SearchBar/SearchBar.js
+++ src/components/UserInterface/SearchBar/SearchBar.js
@@ -1,18 +1,29 @@
+import { useRef } from "react";
 import styles from "./SearchBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SearchBar = (props) => {
+  const ref = useRef("");
+  // console.log(value);
+  const ClickHandler = () => {
+    const value = ref.current.value;
+    console.log(value);
+
+    ref.current.value = "";
+  };
+
   return (
     <div>
       <label htmlFor="search" className={styles.label}>
         <FontAwesomeIcon icon={props.icon} className={styles.icon} />
         <input
+          ref={ref}
           id="search"
           type="text"
           placeholder="Search GitHub username_"
           className={styles.input}
         />
-        <button type="submit" className={styles.btn}>
+        <button type="submit" className={styles.btn} onClick={ClickHandler}>
           Search
         </button>
       </label>
