4e9033db045c9fbaaa8e91ed4680d4e6613400b1
Finn <mailtorufina@gmail.com>
2022-10-26 14:01:20 -0700

    update UserContact.js

--- src/components/SearchResults/UserContact/UserContact.js
+++ src/components/SearchResults/UserContact/UserContact.js
@@ -1,26 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLink,
+  faLocationDot,
+  faBuilding,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import styles from "./UserContact.module.css";
 
-const UserContact = ({
-  contactInfo: { job, location, twitter, website },
-  icons: [locationIcon, twitterIcon, websiteIcon, jobIcon],
-}) => {
+const UserContact = ({ contactInfo: { job, location, twitter, website } }) => {
   const contactData = [
     {
       name: location,
-      icon: locationIcon,
+      icon: faLocationDot,
     },
     {
       name: twitter,
-      icon: twitterIcon,
+      icon: faTwitter,
     },
     {
       name: website,
-      icon: websiteIcon,
+      icon: faLink,
     },
     {
       name: job,
-      icon: jobIcon,
+      icon: faBuilding,
     },
   ];
 
