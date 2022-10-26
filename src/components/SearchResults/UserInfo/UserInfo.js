617b4b569a7de7cba63773c93d6a8bacf3873834
Finn <mailtorufina@gmail.com>
2022-10-26 14:49:39 -0700

    update UserInfo.js

--- src/components/SearchResults/UserInfo/UserInfo.js
+++ src/components/SearchResults/UserInfo/UserInfo.js
@@ -1,17 +1,29 @@
 import styles from "./UserInfo.module.css";
 
-const UserInfo = ({ userInfo: { image, name, handle, bio, date } }) => {
+const UserInfo = ({
+  userInfo: { avatar_url, name, login, bio, created_at },
+}) => {
   return (
     <div className={styles.userInfo}>
-      <img className={styles.userImage} src={image} alt="User avatar" />
+      <img className={styles.userImage} src={avatar_url} alt="User avatar" />
       <div className={styles.userInfoIntro}>
         <div className={styles.userInfoName}>
-          <h2>{name}</h2>
-          <p className={styles.userInfoHandle}>{handle}</p>
+          <h2>{`${name || "Not Available"}`}</h2>
+          <p className={styles.userInfoHandle}>{`${
+            login || "Not Available"
+          }`}</p>
         </div>
-        <p className={styles.userInfoDate}>{date}</p>
+        <p className={styles.userInfoDate}>{`Joined ${
+          new Date(created_at).toLocaleString("en-US", {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+          }) || "Not Available"
+        }`}</p>
       </div>
-      <p className={styles.userInfoBio}>{bio} </p>
+      <p className={styles.userInfoBio}>
+        {`${bio || "This profile has no bio"}`}
+      </p>
     </div>
   );
 };
