d6c483f4a44f70886388576bf4e7bb2da469d115
Finn <mailtorufina@gmail.com>
2022-10-26 14:28:31 -0700

    update UserDisplay.js

--- src/components/UserInterface/UserDisplay/UserDisplay.js
+++ src/components/UserInterface/UserDisplay/UserDisplay.js
@@ -3,12 +3,33 @@ import UserAccount from "../../SearchResults/UserAccount/UserAccount";
 import UserContact from "../../SearchResults/UserContact/UserContact";
 import styles from "./UserDisplay.module.css";
 
-const UserDisplay = ({ icons, userData: { profile, account, contact } }) => {
+const UserDisplay = ({ userData }) => {
+  const {
+    avatar_url,
+    name,
+    login,
+    created_at,
+    bio,
+    public_repos,
+    followers,
+    following,
+    location,
+    twitter_username,
+    blog,
+    company,
+  } = userData;
+
+  const displayData = {
+    profile: [avatar_url, name, login, created_at, bio],
+    account: [public_repos, followers, following],
+    contact: [location, twitter_username, blog, company],
+  };
+  console.log("data: ", userData);
   return (
     <div className={styles.userDisplay}>
-      <UserInfo userInfo={{ ...profile }} />
-      <UserAccount accountInfo={{ ...account }} />
-      <UserContact icons={icons} contactInfo={{ ...contact }} />
+      <UserInfo userInfo={{ ...displayData.profile }} />
+      <UserAccount accountInfo={{ ...displayData.account }} />
+      <UserContact contactInfo={{ ...displayData.contact }} />
     </div>
   );
 };
