e4555a9de37cf0477cad150b02a683424985203c
Finn <mailtorufina@gmail.com>
2022-10-28 06:26:41 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,8 @@ import styles from "./App.module.css";
 import Header from "./components/UserInterface/Header/Header";
 import SearchBar from "./components/UserInterface/SearchBar/SearchBar";
 import UserDisplay from "./components/UserInterface/UserDisplay/UserDisplay";
-import octocat from "./assets/images/octocat.jpg";
 
 function App() {
-  const UserData = {
-    profile: {
-      image: octocat,
-      name: "The Octocat",
-      handle: "@octocat",
-      bio: "This profile has no bio",
-      date: "Joined 25 Jan 2011",
-    },
-    account: {
-      repo: "8",
-      followers: "3938",
-      following: "9",
-    },
-    contact: {
-      location: "San Fransisco",
-      twitter: "Not Available",
-      website: "https://github.blog",
-      job: "github",
-    },
-  };
   const [apiData, setApiData] = useState({});
 
   const fetchUser = async (username) => {
