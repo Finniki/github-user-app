b331d753997f855fade1fb95d2dbcbb67accee71
Finn <mailtorufina@gmail.com>
2022-10-28 06:24:29 -0700

    Take date logic out of return statement

--- src/components/SearchResults/UserInfo/UserInfo.js
+++ src/components/SearchResults/UserInfo/UserInfo.js
@@ -3,9 +3,21 @@ import styles from "./UserInfo.module.css";
 const UserInfo = ({
   userInfo: { avatar_url, name, login, bio, created_at },
 }) => {
+  const formattedDate = created_at
+    ? new Date(created_at).toLocaleString("en-US", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      })
+    : "YY/MM/DD";
+
   return (
     <div className={styles.userInfo}>
-      <img className={styles.userImage} src={avatar_url || octocat} alt="User avatar" />
+      <img
+        className={styles.userImage}
+        src={avatar_url || octocat}
+        alt="User avatar"
+      />
       <div className={styles.userInfoIntro}>
         <div className={styles.userInfoName}>
           <h2>{`${name || "Not Available"}`}</h2>
@@ -13,19 +25,7 @@ const UserInfo = ({
             login || "Not Available"
           }`}</p>
         </div>
-        <p className={styles.userInfoDate}>{`Joined ${
-          const formattedDate = created_at
-    ? new Date(created_at).toLocaleString("en-US", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-      })
-    : "YY/MM/DD";
-            day: "2-digit",
-            month: "short",
-            year: "numeric",
-          }) || "Not Available"
-        }`}</p>
+        <p className={styles.userInfoDate}>{formattedDate}</p>
       </div>
       <p className={styles.userInfoBio}>
         {`${bio || "This profile has no bio"}`}
